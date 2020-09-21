f6f49ba6d4fdc348e4a24f0131ff843e816de373
Sep Vrij <svrij22@gmail.com>
2020-09-21 23:32:35 +0200

    test voor 2 domein klassen

--- src/ovchip_hibernate/dao/AdresDAO.java
+++ src/ovchip_hibernate/dao/AdresDAO.java
@@ -5,7 +5,7 @@ import ovchip_hibernate.domein.Reiziger;
 
 import java.util.List;
 
-public interface AdresDAO extends DefaultDAO{
+public interface AdresDAO extends DefaultDAOHibernate.DefaultDAO {
     Adres findById(int id);
     Adres findByReiziger(Reiziger reiziger);
     List<Adres> findAll();
